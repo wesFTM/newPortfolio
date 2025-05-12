4f986ebe07c80c0ffa20b6467a053269e8d13bd8
wesFTM <wes@firsttubemedia.com>
2025-05-12 15:11:35 -0400

    Fix: remove unused icon imports from Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
-import { FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 export default function Header() {
   const [showVideo, setShowVideo] = useState(false);
